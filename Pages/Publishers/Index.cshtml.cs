b44d9f3d88582bb7aaaa14140b29028deb5e31ed
alexia2929 <132937530+alexia2929@users.noreply.github.com>
2023-10-22 16:24:25 +0300

    Add project files.

---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Muntean_Alexia_Lab2.Data;
+using Muntean_Alexia_Lab2.Models;
+
+namespace Muntean_Alexia_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Muntean_Alexia_Lab2.Data.Muntean_Alexia_Lab2Context _context;
+
+        public IndexModel(Muntean_Alexia_Lab2.Data.Muntean_Alexia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
